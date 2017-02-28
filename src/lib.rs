b7d7737e4394e732d7217467931e44fefc890d18
Jonathan Behrens <behrensj@mit.edu>
2017-02-28 17:36:11 -0500

    Switch from optional 'disable_timekeeper' feature to default 'enable_timekeeper' feature

--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,15 @@ mod source;
 pub use simpletracker::SimpleTracker;
 pub use source::*;
 
-#[cfg(not(feature = "disable_timekeeper"))] mod timer;
-#[cfg(not(feature = "disable_timekeeper"))] mod timerset;
-#[cfg(not(feature = "disable_timekeeper"))] pub use timer::*;
-#[cfg(not(feature = "disable_timekeeper"))] pub use timerset::*;
+#[cfg(feature = "enable_timekeeper")] mod timer;
+#[cfg(feature = "enable_timekeeper")] mod timerset;
+#[cfg(feature = "enable_timekeeper")] pub use timer::*;
+#[cfg(feature = "enable_timekeeper")] pub use timerset::*;
 
-#[cfg(feature = "disable_timekeeper")] mod nop_timer;
-#[cfg(feature = "disable_timekeeper")] mod nop_timerset;
-#[cfg(feature = "disable_timekeeper")] pub use nop_timer::*;
-#[cfg(feature = "disable_timekeeper")] pub use nop_timerset::*;
+#[cfg(not(feature = "enable_timekeeper"))] mod nop_timer;
+#[cfg(not(feature = "enable_timekeeper"))] mod nop_timerset;
+#[cfg(not(feature = "enable_timekeeper"))] pub use nop_timer::*;
+#[cfg(not(feature = "enable_timekeeper"))] pub use nop_timerset::*;
 
 pub trait Tracker: Default {
     type Statistics: Default;
